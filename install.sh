5b196059671813ea7826c32b8b2bdd6d1594771a
Shogo Ishikura <ishikura.shogo@gmail.com>
2018-05-09 01:06:05 +0900

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,161 @@
 #!/bin/bash
+setup_mac() {
+	if [ "$(uname)" == 'Darwin' ]; then
+		## Mac
+		echo "Setup for Mac"
+		which brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	fi
+}
+
+setup_mac_cask() {
+	if [ "$(uname)" == 'Darwin' ]; then
+		## Mac
+		echo "Setup Cask for Mac"
+
+		casks=(
+		iterm2
+		dropbox
+		virtualbox
+		vagrant
+		vagrant-manager
+		docker
+		docker-toolbox
+		gas-mask
+		bettertouchtool
+		alfred
+		slack
+		google-chrome
+		)
+
+		echo "start brew cask install apps..."
+		for cask in "${casks[@]}"; do
+			echo "---------------------------"
+			echo "installing ${cask}"
+			echo "---------------------------"
+			brew cask install $cask
+			echo ""
+		done
+
+		brew cask cleanup
+	fi
+}
+
+setup_debian() {
+	if   [ -e /etc/debian_version ] || [ -e /etc/debian_release ]; then
+		# Check Ubuntu or Debian
+		if [ -e /etc/lsb-release ]; then
+			# Ubuntu
+			# Not implemented yet
+			echo "Setup for Ubuntu"
+			sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+            test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
+            test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
+            test -r ~/.bash_profile && echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.bash_profile
+            echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.profile
+		else
+			# Debian
+			# Not implemented yet
+			echo "Setup for Debian"
+			sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+            test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
+            test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
+            test -r ~/.bash_profile && echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.bash_profile
+            echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.profile
+		fi
+	fi
+}
+
+setup_redhat() {
+	if [ -e /etc/fedora-release ]; then
+		# Fedra
+		echo "Setup for Fedora"
+		sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+		test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
+		test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
+		test -r ~/.bash_profile && echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.bash_profile
+		echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.profile
+	elif [ -e /etc/redhat-release ]; then
+		if [ -e /etc/oracle-release ]; then
+			# Oracle Linux
+			echo "Setup for Oracle Linux"
+			sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+            test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
+            test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
+            test -r ~/.bash_profile && echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.bash_profile
+            echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.profile
+		else
+			# Red Hat Enterprise Linux
+			echo "Setup for Red Hat Enterprise Linux"
+			sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+            test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
+            test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
+            test -r ~/.bash_profile && echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.bash_profile
+            echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.profile
+		fi
+	fi
+}
+
+setup_arch() {
+	if [ -e /etc/arch-release ]; then
+		# Arch Linux
+		echo "Setup for Arch Linux"
+		sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+		test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
+		test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
+		test -r ~/.bash_profile && echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.bash_profile
+		echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.profile
+	fi
+}
+
+setup_suse() {
+	if [ -e /etc/SuSE-release ]; then
+		# OpenSUSE
+		echo "Setup for OpenSUSE"
+		sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+		test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
+		test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
+		test -r ~/.bash_profile && echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.bash_profile
+		echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.profile
+	fi
+}
+
+setup_mandriva() {
+	if [ -e /etc/mandriva-release ]; then
+		# Mandriva Linux
+		echo "Setup for Mandriva Linux"
+		sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+		test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
+		test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
+		test -r ~/.bash_profile && echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.bash_profile
+		echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.profile
+	fi
+}
+
+setup_gentoo() {
+	if [ -e /etc/gentoo-release ]; then
+		# Gentoo Linux
+		echo "Setup for Gentoo Linux"
+		sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+		test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
+		test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
+		test -r ~/.bash_profile && echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.bash_profile
+		echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.profile
+	fi
+}
+
+brewinstall() {
+	setup_mac
+	setup_debian
+	setup_redhat
+	setup_arch
+	setup_arch
+	setup_suse
+	setup_mandriva
+	setup_gentoo
+}
 
 echo "installing homebrew..."
-which brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brewinstall
 
 echo "run brew doctor..."
 which brew >/dev/null 2>&1 && brew doctor
@@ -30,7 +184,6 @@ formulas=(
     python3
     lua
     "vim --with-lua --with-python3"
-    ricty
     markdown
     ssh-copy-id
     diff-so-fancy
@@ -52,32 +205,9 @@ echo "---------------------------"
 pip3 install --upgrade neovim
 echo ""
 
-casks=(
-    iterm2
-    dropbox
-    virtualbox
-    vagrant
-    vagrant-manager
-    docker
-    docker-toolbox
-    gas-mask
-    bettertouchtool
-    alfred
-    slack
-    google-chrome
-)
-
-echo "start brew cask install apps..."
-for cask in "${casks[@]}"; do
-    echo "---------------------------"
-    echo "installing ${cask}"
-    echo "---------------------------"
-    brew cask install $cask
-    echo ""
-done
-
 brew cleanup
-brew cask cleanup
+
+setup_mac_cask
 
 cat << END
 
